a6f6754d3129433a37e6b451c27272ec3b3b50b8
weixin_44368000 <weixin_44368000@gitcode.net>
2024-03-07 15:11:52 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import App from './App'
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+// 引入全局TuniaoUI
+import TuniaoUI from 'tuniao-ui'
+// 引入store
+import store from './store'
+// 引入TuniaoUI提供的vuex简写方法
+let vuexStore = require('@/store/$tn.mixin.js')
+Vue.mixin(vuexStore)
+Vue.use(TuniaoUI)
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+	store,
+  ...App
+})
+app.$mount()
+
